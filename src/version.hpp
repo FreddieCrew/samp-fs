8fe36a7399265fe18776047f7d6ffe60259a3875
John M. Reyes <120303127+FreddieCrew@users.noreply.github.com>
2024-02-16 03:30:34 -0700

    Bump plugin version

--- src/version.hpp
+++ src/version.hpp
@@ -2,6 +2,6 @@
 #ifndef FS_VERSION_H
 #define FS_VERSION_H
 
-#define INCLUDE_VERSION (0x101)
-#define PLUGIN_VERSION "1.0.1"
+#define INCLUDE_VERSION (0x102)
+#define PLUGIN_VERSION "1.0.2"
 #endif
